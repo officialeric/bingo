e4ddcdd2bd2e264ad7e7ad5c0d1caa0769dc012a
officialeric <officialeric994@gmail.com>
2024-08-11 10:24:09 +0300

    refreshing.

--- database/bingo.sql
+++ database/bingo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 07, 2024 at 09:34 AM
+-- Generation Time: Aug 11, 2024 at 09:23 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -41,6 +41,63 @@ CREATE TABLE `admins` (
 INSERT INTO `admins` (`id`, `username`, `email`, `password`) VALUES
 (1, 'admin', 'admin@bingo.com', '123');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applications`
+--
+
+CREATE TABLE `applications` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `region` varchar(100) NOT NULL,
+  `district` varchar(100) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `marital_status` enum('single','married') NOT NULL,
+  `education_level` varchar(100) NOT NULL,
+  `is_working` enum('Yes','No') NOT NULL,
+  `work_place` varchar(255) DEFAULT NULL,
+  `has_traveled` enum('Yes','No') NOT NULL,
+  `traveled_countries` text DEFAULT NULL,
+  `has_applied` enum('Yes','No') NOT NULL,
+  `applied_countries` text DEFAULT NULL,
+  `preferences` text NOT NULL,
+  `has_passport` enum('Yes','No') NOT NULL,
+  `passport` varchar(255) DEFAULT NULL,
+  `cv` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `applications`
+--
+
+INSERT INTO `applications` (`id`, `name`, `email`, `phone_number`, `age`, `country`, `region`, `district`, `street`, `marital_status`, `education_level`, `is_working`, `work_place`, `has_traveled`, `traveled_countries`, `has_applied`, `applied_countries`, `preferences`, `has_passport`, `passport`, `cv`, `created_at`) VALUES
+(6, 'Eric Ernest', 'officialeric994@gmail.com', '626370989', 20, 'Tanzania', 'Dar', 'Ilala', 'upanga', 'single', 'Ordinary Diploma', 'No', '', 'No', '', 'No', '', 'Tanzania', 'Yes', '66b861727d7bb_Assignment One.pdf', '66b861727d7e5_Assignment One.pdf', '2024-08-11 07:00:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `heading` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `heading`, `description`) VALUES
+(1, 'Welcome new user', 'here you can apply any job you want');
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +109,18 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,6 +128,18 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `applications`
+--
+ALTER TABLE `applications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
